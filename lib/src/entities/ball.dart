247fdc2b5df805dba96ad84f04bc6caa8ad75016
matt <mkc11267@gmail.com>
2024-06-28 11:26:06 +0800

    Remove balls on edge

--- lib/src/entities/ball.dart
+++ lib/src/entities/ball.dart
@@ -10,14 +10,41 @@ class Ball extends PositionComponent with HasGameRef<BallGame> {
 
   final Vector2 initialPosition;
   final radius = 10.0;
+  final speed = 50.0;
+
+  late final direction = (Vector2.random() - Vector2.random()).normalized() * speed;
 
   @override
   FutureOr<void> onLoad() {
+    print('Ball loaded $hashCode');
     position = initialPosition;
     anchor = Anchor.center;
     size = Vector2.all(radius);
     final paint = Paint()..color = Colors.primaries[Random().nextInt(Colors.primaries.length)];
     final circle = CircleComponent(radius: radius, paint: paint);
     add(circle);
+  }
+
+  @override
+  void onRemove() {
+    print('Ball removed $hashCode');
+    super.onRemove();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position += direction * dt;
+    _removeOnScreenEdge();
+  }
+
+  // Remove ball if it moves off screen
+  void _removeOnScreenEdge() {
+    if (position.x < -radius ||
+        position.x > gameRef.size.x + radius ||
+        position.y < -radius ||
+        position.y > gameRef.size.y + radius) {
+      removeFromParent();
+    }
   }
 }
